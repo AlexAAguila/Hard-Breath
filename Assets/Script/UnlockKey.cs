37f9a47c449e45b85dadd3ca448b7cbb4da8b2ab
Clay <clayton_cheung@sfu.ca>
2021-03-15 21:41:49 -0700

    update script

--- Assets/Script/UnlockKey.cs
+++ Assets/Script/UnlockKey.cs
@@ -26,12 +26,18 @@ keyanimation = GameObject.Find("rust_key").GetComponent<Animator>();
       //if key exists
     if(key != null){
       //get the rotation value from each statues
-               var rotation = UnityEditor.TransformUtils.GetInspectorRotation(statue[0].transform).y;
-               var rotation2 = UnityEditor.TransformUtils.GetInspectorRotation(statue[1].transform).y;
-                var rotation3 = UnityEditor.TransformUtils.GetInspectorRotation(statue[2].transform).y;
-                 var rotation4 = UnityEditor.TransformUtils.GetInspectorRotation(statue[3].transform).y;
+      float rotation=0;
+      float sum =0;
+      for(int i=0;i<4;i++){
+        rotation = UnityEditor.TransformUtils.GetInspectorRotation(statue[i].transform).y;
+        sum +=rotation;
+      }
+              //  var rotation = UnityEditor.TransformUtils.GetInspectorRotation(statue[0].transform).y;
+              //  var rotation2 = UnityEditor.TransformUtils.GetInspectorRotation(statue[1].transform).y;
+              //   var rotation3 = UnityEditor.TransformUtils.GetInspectorRotation(statue[2].transform).y;
+              //    var rotation4 = UnityEditor.TransformUtils.GetInspectorRotation(statue[3].transform).y;
                  //get the sum of the rotation values
-               var sum = rotation+ rotation2+rotation3+rotation4;
+              //  var sum = rotation+ rotation2+rotation3+rotation4;
                Debug.Log(sum);
     //check if all the statue  is facing the exit
 if(rotation <= -90 && rotation >= -91 && sum<= -360 && sum >= -361 ){
