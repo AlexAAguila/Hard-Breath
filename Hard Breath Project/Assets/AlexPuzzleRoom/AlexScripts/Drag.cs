749625cc3dee7531889f9419c148bc5c160fae10
Alex <alex_a_aguilar@outlook.com>
2021-03-16 23:57:04 -0700

    Latest one

---
+++ Hard Breath Project/Assets/AlexPuzzleRoom/AlexScripts/Drag.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Drag : MonoBehaviour
+{
+    // Start is called before the first frame update
+    //Reference: https://www.youtube.com/watch?v=Xv-c3-IOnM0&t
+    //I didn't use all of his code, plus I changed conditions and set Inactive assests to Active when conditions were met
+
+    public Transform player; // Reference to player
+    public Transform playerCam;// Follows camera's position
+    bool hasPlayer = false;// If player is in range
+    bool beingCarried = false; //If the item is being carried
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float dist = Vector3.Distance(gameObject.transform.position, player.position);
+        if (dist <= 2.5f)
+        {
+            hasPlayer = true; //Player is in range and condition is met to pick up the item
+
+        }
+        else
+        {
+            hasPlayer = false;
+        }
+        if (hasPlayer && Input.GetMouseButtonDown(0)) //If player is in range and the Left mouse key is pressed the player can pick up the item
+        {
+            GetComponent<Rigidbody>().isKinematic = true;
+            transform.parent = playerCam;//This causes the item to be the child of the player
+            beingCarried = true;
+            this.transform.GetChild(3).gameObject.SetActive(true);//Sets the bottle colour asset to active
+        }
+        else if (beingCarried && Input.GetMouseButtonDown(1)) //If left mouse key is pressed while the item is being carried, then the item is dropped
+        {
+            beingCarried = false;
+            hasPlayer = false;
+            transform.parent = null;// The item is no longer a child of the player
+            GetComponent<Rigidbody>().isKinematic = false;
+            this.transform.GetChild(3).gameObject.SetActive(false);//Sets the bottle colour asset to inactive
+
+        }
+
+
+
+
+    }
+}
---
+++ Hard Breath Project/Assets/AlexPuzzleRoom/AlexScripts/PotConditions.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PotConditions : MonoBehaviour
+{
+    //I made this code myself
+    //This code is designed so that conditions need to be met in order to leave the room
+    //The bottles are the only assets that have triggers so if both triggers go in the pot
+    //then the condition is fulfilled
+
+
+    public float amountOfBottles = 0;
+    public AudioSource poor;
+    // Start is called before the first frame update
+    void Start()
+    {
+        poor = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       if (amountOfBottles == 2) // Once both bottles are in then the condition is complete
+        {
+            Debug.Log("BOTTTLES!!!!");
+            
+            Destroy(this);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(other.gameObject);
+       amountOfBottles++; // Adding numbers of bottles
+       Debug.Log("Bottle num = " + amountOfBottles);
+        poor.Play();
+    }
+
+  
+}
