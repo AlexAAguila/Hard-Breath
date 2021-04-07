4b60f3412bf2f57f4d3cd84ff2a85cb0ea0ffb52
Alex Aguilar <Alex_a_aguilar@outlook.com>
2021-04-06 19:51:12 -0700

    Clayton's puzzle

---
+++ Hard Breath Project/Assets/Clayton folder/Script/ChestKey.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChestKey : MonoBehaviour
+{
+    //variable
+     public GameObject player;
+    public bool keyCollect = false;
+
+      //start function to find gameobject and its component
+    void Start()
+    {
+        player = GameObject.Find("FPSController");
+    }
+
+    // Update function to destroy the key object if the user collide to it
+    void Update()
+    {
+         if ((player.transform.position - this.transform.position).sqrMagnitude < 2*2){
+             
+             Destroy(gameObject);
+            
+   keyCollect = true;
+    }
+   
+    }
+}
---
+++ Hard Breath Project/Assets/Clayton folder/Script/Rotate.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+  //variable
+    public GameObject player;
+  public float rotation;
+    void Start(){
+      player = GameObject.Find("FPSController");
+     
+      
+    }
+//on mouse down to rotate the statues
+  void OnMouseDown(){
+    //check if the player in range
+       if ((player.transform.position - this.transform.position).sqrMagnitude < 3*3){
+         //rotate statue
+           transform.Rotate(0, 90f, 0);
+           //get the roation value
+          rotation = UnityEditor.TransformUtils.GetInspectorRotation(this.transform).y;
+          rotation+=rotation;
+  
+      }
+
+           
+    
+}
+}
