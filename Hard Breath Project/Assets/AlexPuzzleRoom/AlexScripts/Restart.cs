f7699f36df90decf018b0bd5887cc7347d234574
Alex Aguilar <Alex_a_aguilar@outlook.com>
2021-04-07 23:33:10 -0700

    Alex's Puzzle Updated

---
+++ Hard Breath Project/Assets/AlexPuzzleRoom/AlexScripts/Restart.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Restart : MonoBehaviour
+{
+    public Vector3 startPosition;
+    //public GameObject player;
+    public GameObject warp;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPosition = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject == warp)
+        {
+            transform.position = startPosition;
+            Debug.Log("passed");
+        }
+    }
+}
