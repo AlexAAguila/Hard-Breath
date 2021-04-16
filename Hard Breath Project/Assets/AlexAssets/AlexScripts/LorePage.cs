997cdcaaaabc0cd0e7beedda10fc3ec5aacad5d4
Alex Aguilar <Alex_a_aguilar@outlook.com>
2021-04-15 21:21:51 -0700

    Lore shows up when you go near the paper in empty room

---
+++ Hard Breath Project/Assets/AlexAssets/AlexScripts/LorePage.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LorePage : MonoBehaviour
+
+
+{
+
+    public Transform player; // Reference to player
+    public GameObject paper;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float dist = Vector3.Distance(gameObject.transform.position, player.position);
+        if (dist <= 1.5f)
+        {
+            paper.SetActive(true);
+        }
+        else
+        {
+            paper.SetActive(false);
+        }
+
+   
+    }
+}
